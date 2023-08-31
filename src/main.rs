9786aa573bc0364bdaa0c7c67caa0627affdbd2b
FuzzyForest <red.hope6833@fastmail.com>
2023-08-31 21:08:25 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,99 @@
-use std::{iter::Peekable, str::Chars};
+use anyhow::{bail, Context, Result};
+use std::{
+    iter::{Enumerate, Peekable},
+    str::Chars,
+};
+
+#[derive(Debug)]
+struct Token {
+    value: String,
+    position: usize,
+}
 
 struct TokenIterator<'a> {
-    input: Peekable<Chars<'a>>,
+    input: Peekable<Enumerate<Chars<'a>>>,
 }
 
 impl<'a> Iterator for TokenIterator<'a> {
-    type Item = String;
+    type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while self.input.next_if(|c| c.is_whitespace()).is_some() {}
+        while self.input.next_if(|c| c.1.is_whitespace()).is_some() {}
 
         // NOTE: The first character is not whitespace
         match self.input.peek() {
-            Some('(') => {
-                self.input.next();
-                Some("(".to_owned())
-            }
-            Some(')') => {
-                self.input.next();
-                Some(")".to_owned())
-            }
-            Some(';') => {
+            Some((_, '(' | ')')) => self.input.next().map(|c| Token {
+                value: c.1.into(),
+                position: c.0,
+            }),
+            Some((position, ';')) => {
+                let position = *position;
                 let mut comment_token = String::new();
-                while let Some(c) = self.input.next_if(|c| *c != '\n') {
-                    comment_token.push(c)
+                while let Some(c) = self.input.next_if(|c| c.1 != '\n') {
+                    comment_token.push(c.1)
                 }
-                Some(comment_token)
+                Some(Token {
+                    value: comment_token,
+                    position,
+                })
             }
-            // TODO: Parse tokens with whitespace delimited by ""
-            Some(_) => {
+            Some((position, '"')) => {
+                let position = *position;
+                let mut multi_word_token = "\"".to_owned();
+                self.input.next();
+                loop {
+                    match self.input.peek() {
+                        Some((_, '\\')) => {
+                            self.input.next();
+                            match self.input.peek() {
+                                Some((_, '\"')) => {
+                                    multi_word_token.push('"');
+                                    self.input.next();
+                                }
+                                Some((_, '\\')) => {
+                                    multi_word_token.push('\\');
+                                    self.input.next();
+                                }
+                                Some((_, c)) => {
+                                    multi_word_token.push('\\');
+                                    multi_word_token.push(*c);
+                                    self.input.next();
+                                }
+                                None => {
+                                    multi_word_token.push('\\');
+                                }
+                            }
+                        }
+                        Some((_, '"')) => {
+                            multi_word_token.push('"');
+                            self.input.next();
+                            break;
+                        }
+                        Some((_, c)) => {
+                            multi_word_token.push(*c);
+                            self.input.next();
+                        }
+                        None => break,
+                    }
+                }
+                Some(Token {
+                    value: multi_word_token,
+                    position,
+                })
+            }
+            Some((position, _)) => {
+                let position = *position;
                 let mut token = String::new();
                 while let Some(c) = self
                     .input
-                    .next_if(|c| !(c.is_whitespace() || "()".contains(*c)))
+                    .next_if(|c| !(c.1.is_whitespace() || "()".contains(c.1)))
                 {
-                    token.push(c)
+                    token.push(c.1)
                 }
-                Some(token)
+                Some(Token {
+                    value: token,
+                    position,
+                })
             }
             None => None,
         }
@@ -45,12 +102,42 @@ impl<'a> Iterator for TokenIterator<'a> {
 
 fn tokenize<'a>(input: &'a str) -> TokenIterator {
     TokenIterator {
-        input: input.chars().peekable(),
+        input: input.chars().enumerate().peekable(),
     }
 }
 
-fn main() {
-    let program = "(define double (lambda (x) (* x 2))) ; test comment\n (* 2 3)";
-    let tokens: Vec<_> = tokenize(program).collect();
-    dbg!(tokens);
+#[derive(Debug)]
+enum Expression {
+    Atom(String),
+    List(Vec<Expression>),
+}
+
+impl Expression {
+    fn parse<I>(tokens: &mut Peekable<I>) -> Result<Expression>
+    where
+        I: Iterator<Item = Token>,
+    {
+        match tokens.next() {
+            Some(token) if token.value == "(" => {
+                let mut expressions = Vec::new();
+                while !matches!(tokens.peek(), Some(token) if token.value == ")") {
+                    expressions.push(Expression::parse(tokens).with_context(|| {
+                        format!("While parsing list that began at {}", token.position)
+                    })?);
+                }
+                tokens.next();
+                Ok(Expression::List(expressions))
+            }
+            Some(token) => return Ok(Expression::Atom(token.value.clone())),
+            None => bail!("Ran out of tokens"),
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let program = r#"(+ (* 2 3) 4)"#;
+    let mut tokens = tokenize(program).peekable();
+    dbg!(Expression::parse(&mut tokens)?);
+    dbg!(tokens.collect::<Vec<_>>());
+    Ok(())
 }
